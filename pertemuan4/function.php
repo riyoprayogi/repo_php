6613ba4e6463bd4af290b66a832c2d666c6d2df1
Ryoリズキー <74493992+riyoprayogi@users.noreply.github.com>
2021-07-17 21:08:05 +0800

    Update function.php

--- pertemuan4/function.php
+++ pertemuan4/function.php
@@ -1,20 +1,38 @@
 <?php
-function salam($waktu, $nama = "Admin") {
-    return "Selamat , $nama!";
+function salam($waktu = "Datang", $nama = "Admin") {
+  $b = time();
+
+  $hour = date("g", $b);
+  $m    = date("A", $b);
+
+  if ($m == "AM") {
+    if ($hour == 12) {
+      $waktu = "Evening";
+    } elseif ($hour < 4) {
+      $waktu = "Evening";
+    } elseif ($hour > 3) {
+      $waktu = "Morning";
+    }
+  }
+
+      elseif ($m == "PM") {
+    if ($hour == 12) {
+      $waktu = "Afternoon";
+    } elseif ($hour < 6) {
+      $waktu = "Afternoon";
+    } elseif ($hour > 5) {
+      $waktu = "Evening";
+    }
+  }
+  return "Good $waktu, $nama";
 }
-
-$waktu = date("g:i:s A", time());
-echo $waktu;
-
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Latihan Function</title>
+  <title>Test</title>
 </head>
 <body>
-    <h1><?=salam($waktu, "Riyo"); ?></h1>
+  <h1><?php echo salam("","Riyo!"); ?></h1>
 </body>
 </html>
