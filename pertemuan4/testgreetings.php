54020477f729c1739c82534d44131549d8fe015a
Ryoリズキー <74493992+riyoprayogi@users.noreply.github.com>
2021-07-17 21:07:29 +0800

    Update testgreetings.php

--- pertemuan4/testgreetings.php
+++ pertemuan4/testgreetings.php
@@ -1,41 +1,38 @@
 <?php
-function salam($m, $nama = "Admin") {
-  return "Selamat , $nama!";
+function salam($waktu = "Datang", $nama = "Admin") {
+  $b = time();
+
+  $hour = date("g", $b);
+  $m    = date("A", $b);
+
+  if ($m == "AM") {
+    if ($hour == 12) {
+      $waktu = "Evening";
+    } elseif ($hour < 4) {
+      $waktu = "Evening";
+    } elseif ($hour > 3) {
+      $waktu = "Morning";
+    }
+  }
+
+      elseif ($m == "PM") {
+    if ($hour == 12) {
+      $waktu = "Afternoon";
+    } elseif ($hour < 6) {
+      $waktu = "Afternoon";
+    } elseif ($hour > 5) {
+      $waktu = "Evening";
+    }
+  }
+  return "Good $waktu, $nama";
 }
-
-    $b = time();
-
-    $hour = date("g", $b);
-    $m    = date("A", $b);
-
-    if ($m == "AM") {
-      if ($hour == 12) {
-        echo "Good Evening!";
-      } elseif ($hour < 4) {
-        echo "Good Evening!";
-      } elseif ($hour > 3) {
-        echo "Good Morning!";
-      }
-    }
-
-        elseif ($m == "PM") {
-      if ($hour == 12) {
-        echo "Good Afternoon!";
-      } elseif ($hour < 6) {
-        echo "Good Afternoon!";
-      } elseif ($hour > 5) {
-        echo "Good Evening!";
-      }
-    }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Latihan Function</title>
+  <title>Test</title>
 </head>
 <body>
-    <h1><?=salam("","Riyo"); ?></h1>
+  <h1><?php echo salam("","Riyo!"); ?></h1>
 </body>
 </html>
