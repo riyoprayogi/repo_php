c592d7fea2866da488aa34c00af7ac35923dcae2
Ryoリズキー <74493992+riyoprayogi@users.noreply.github.com>
2022-01-08 17:13:58 +0800

    Add files via upload

---
+++ pertemuan5/latihan1.php
@@ -0,0 +1,33 @@
+<?php 
+// Array
+// variabel yang dapat memiliki banyak nilai
+// elemen pada array booelh meiliki tipe data yang berbeda
+// pasangan antara key dan value
+// key-nya adalah index, yang dimulai dari 0
+
+
+// membuat array 
+// cara lama
+$hari = array("Senin", "Selasa", "Rabu");
+$bulan = ["Januari", "Februari", "Maret"];
+$arrl = [123, "tulisan", false];
+
+// Menampilkan array (untuk debug, bukan untuk user)
+// var_dump() / print_r()
+// var_dump($hari);
+// echo "<br>";
+// print_r($bulan);
+// echo "<br>";
+// // Menampilkan 1 elemen pada array
+// echo $arrl[0];
+// echo "<br>";
+// echo $bulan[0];
+
+// Menambahkan elemen baru pada array
+var_dump($hari);
+$hari[] = "Kamis";
+$hari[] = "Jum'at";
+echo "<br>";
+var_dump($hari);
+
+?>
---
+++ pertemuan5/latihan2.php
@@ -0,0 +1,29 @@
+<?php
+// Pengulangan pada array
+// for / foreach
+$angka = [3,2,15,20,11,77,89];
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>latihan 2</title>
+    <style>
+        div {
+            width: 50px;
+			height: 50px;
+			background-color: salmon;
+			text-align: center;
+			line-height: 50px;
+			margin: 3px;
+            float: left; 
+        }
+    </style>
+</head>
+<body>
+    <div>3</div>
+
+
+    
+</body>
+</html>
